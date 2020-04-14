5f4b01dd4e78cf6d1685c53fef1e4692923eeea9
MaK <42501101+Mouzayan@users.noreply.github.com>
2020-04-14 16:15:20 -0400

    Item create feature included

--- controllers/index.js
+++ controllers/index.js
@@ -1,13 +1,13 @@
-const bcrypt = require('bcrypt');
-const jwt = require('jsonwebtoken');
-const User = require('../models/user');
-const Item = require('../models/item');
-const db = require('../db');
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const User = require("../models/user");
+const Item = require("../models/item");
+const db = require("../db");
 
-db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+db.on("error", console.error.bind(console, "MongoDB connection error:"));
 
 const SALT_ROUNDS = 11;
-const TOKEN_KEY = 'areallylonggoodkey';
+const TOKEN_KEY = "areallylonggoodkey";
 
 const signUp = async (req, res) => {
   try {
@@ -31,7 +31,7 @@ const signUp = async (req, res) => {
     return res.status(201).json({ user, token });
   } catch (error) {
     console.log(
-      'You made it to the signUp controller, but there was an error :('
+      "You made it to the signUp controller, but there was an error :("
     );
     return res.status(400).json({ error: error.message });
   }
@@ -51,7 +51,7 @@ const signIn = async (req, res) => {
       const token = jwt.sign(payload, TOKEN_KEY);
       return res.status(201).json({ user, token });
     } else {
-      res.status(401).send('Invalid Credentials');
+      res.status(401).send("Invalid Credentials");
     }
   } catch (error) {
     return res.status(500).json({ error: error.message });
@@ -60,11 +60,11 @@ const signIn = async (req, res) => {
 
 const verifyUser = (req, res) => {
   try {
-    const token = req.headers.authorization.split(' ')[1];
+    const token = req.headers.authorization.split(" ")[1];
     const user = jwt.verify(token, TOKEN_KEY);
     res.json({ user });
   } catch (e) {
-    res.status(401).send('Not Authorized');
+    res.status(401).send("Not Authorized");
   }
 };
 
@@ -73,7 +73,7 @@ const changePassword = async (req, res) => {};
 const getItems = async (req, res) => {
   try {
     const { _id } = req.body;
-    const items = await User.findById({ _id }).populate('items');
+    const items = await User.findById({ _id }).populate("items");
     res.json(items);
   } catch (error) {
     res.status(500).json({ error: error.message });
@@ -88,7 +88,7 @@ const getItem = async (req, res) => {
     if (item) {
       return res.json(item);
     }
-    res.status(404).json({ message: 'Item not found!' });
+    res.status(404).json({ message: "Item not found!" });
   } catch (error) {
     res.status(500).json({ error: error.message });
   }
@@ -96,9 +96,14 @@ const getItem = async (req, res) => {
 
 const createItem = async (req, res) => {
   try {
-    // Find the user and update the users items
-    const item = await new Item(req.body);
+    const { _id, ...rest } = req.body;
+    const item = await new Item(rest);
     await item.save();
+    await User.findByIdAndUpdate(
+      { _id },
+      { $push: { items: item._id } },
+      { upsert: true }
+    );
     res.status(201).json(item);
   } catch (error) {
     console.log(error);
@@ -113,7 +118,7 @@ const updateItem = async (req, res) => {
       return res.status(500).json({ error: error.message });
     }
     if (!item) {
-      return res.status(404).json({ message: 'Item not found!' });
+      return res.status(404).json({ message: "Item not found!" });
     }
     res.status(200).json(item);
   });
@@ -124,9 +129,9 @@ const deleteItem = async (req, res) => {
     const { id } = req.params;
     const deleted = await Item.findByIdAndDelete(id);
     if (deleted) {
-      return res.status(200).send('Item deleted');
+      return res.status(200).send("Item deleted");
     }
-    throw new Error('Item not found');
+    throw new Error("Item not found");
   } catch (error) {
     res.status(500).json({ error: error.message });
   }
