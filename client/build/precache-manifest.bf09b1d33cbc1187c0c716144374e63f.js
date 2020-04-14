67a946295e459f9719785d828e6f3ba82b9ae9e1
Jose Ferreira <jferreira590@gmail.com>
2020-04-14 14:18:36 -0400

    Changed API Config to production URL.

---
+++ client/build/precache-manifest.bf09b1d33cbc1187c0c716144374e63f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "87e375009f4db957ab07b4d600bde62f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f3ea351fffacf7ac8000",
+    "url": "/static/css/main.9686cf61.chunk.css"
+  },
+  {
+    "revision": "c72dc0809f797fd323de",
+    "url": "/static/js/2.d2a1a49b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.d2a1a49b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f3ea351fffacf7ac8000",
+    "url": "/static/js/main.38ab013e.chunk.js"
+  },
+  {
+    "revision": "a15422c73947459aac30",
+    "url": "/static/js/runtime-main.2b4c64c5.js"
+  }
+]);
