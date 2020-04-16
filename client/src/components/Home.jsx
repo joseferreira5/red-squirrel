6e3214a8901142e8fc363a59e1b8119ab1487c8f
MaK <42501101+Mouzayan@users.noreply.github.com>
2020-04-16 15:45:25 -0400

    homepage first commit

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -1,11 +1,19 @@
-import React from 'react';
-import './Home.css';
-import Layout from './shared/Layout';
+import React from "react";
+import "./Home.css";
+import Layout from "./shared/Layout";
 
 const Home = (props) => {
   return (
     <Layout user={props.user}>
-      <div className="home"></div>
+      <div className="about">
+        <p className="homeText">
+        <span className="appName">Red Squirrel</span> is a visual inventory management and tracking software
+          that allows users to input items and their details as well as track changes to the quantities of the items. We provide a simple interfacefor taking the guesswork out of managing your inventory.
+          <span className="callToAction">Sign up now!</span> and maintain a
+          balance between too much and too little inventory. Color codes
+          highlight levels in your inventory that need replenishment.
+        </p>
+      </div>
     </Layout>
   );
 };
