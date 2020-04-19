c92a7f872cd4dd79ba9cb9ccac43237836d96d2f
MaK <42501101+Mouzayan@users.noreply.github.com>
2020-04-19 09:05:28 -0400

    css updates

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -7,15 +7,9 @@ const Home = (props) => {
     <Layout user={props.user}>
       <div className="about">
         <p className="homeText">
-          <span className="appName">Red Squirrel</span> is a visual inventory
-          management and tracking software that allows you to input your stock
-          items and their details as well as track quantity changes. Our app
-          provides a simple interface for taking the guesswork out of managing
-          your inventory.
+          <span className="appName">Red Squirrel</span> takes the guesswork out of managing your inventory.
           <br />
-          <span className="callToAction">Sign up now!</span> and maintain a
-          balance between too much and too little stock. Our color coding system
-          alerts you when levels are low and items need to be replenished.
+          <span className="callToAction">Sign up now!</span> and track quantity changes and
         </p>
       </div>
     </Layout>
