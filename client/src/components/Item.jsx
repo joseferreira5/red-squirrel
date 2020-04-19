7e2fd63ef2c2bf73e697759e3d7b0e78a4c6cc2d
Mike Reidy <mreidy88@gmail.com>
2020-04-19 16:36:22 -0400

    Flags for real this time

--- client/src/components/Item.jsx
+++ client/src/components/Item.jsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React from 'react';
 import { Link } from 'react-router-dom';
 import Flag from "../Images/flag.png";
@@ -19,19 +18,6 @@ const Item = (props) => {
       <Link className="item" to={`/items/detail/${props.itemId}`}>
         { isCritical(props.onHandQty, props.preferredQty) ? <img className="flag" src={Flag}/>  : false} 
         <img className="item-image" src={props.imgURL} alt={props.name} />
-=======
-import React from "react";
-import { Link } from "react-router-dom";
-import mascot from "../Images/MascotWAppReds.png";
-
-import "./Item.css";
-
-const Item = props => {
-  return (
-    <>
-      <Link className="item" to={`/items/detail/${props.itemId}`}>
-        <img className="item-image" src={props.imgURL || mascot} alt={""} />
->>>>>>> a148bbdb6999b1549d5ee1e9204979dd53c4ca0a
         <div className="item-name">{props.name}</div>
         <div className="onhand-qty-header">On Hand Quantity</div>
         <div className="onhand-qty">{props.onHandQty}</div>
