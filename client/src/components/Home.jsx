68249bec7eaf01670be7facb56c577e503d78c77
MaK <42501101+Mouzayan@users.noreply.github.com>
2020-04-19 14:07:37 -0400

    css updates

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -1,15 +1,20 @@
 import React from "react";
 import "./Home.css";
 import Layout from "./shared/Layout";
+import { Link } from "react-router-dom";
 
 const Home = (props) => {
   return (
     <Layout user={props.user}>
       <div className="about">
         <p className="homeText">
-          <span className="appName">Red Squirrel</span> takes the guesswork out of managing your inventory.
+          <span className="appName">Red Squirrel</span> takes the guesswork out
+          of managing your inventory.
           <br />
-          <span className="callToAction">Sign up now!</span> and track quantity changes and
+          <Link className="callToAction" to={`/sign-up/`}>
+            Sign up now!
+          </Link>{" "}
+          and begin tracking your stuff.
         </p>
       </div>
     </Layout>
