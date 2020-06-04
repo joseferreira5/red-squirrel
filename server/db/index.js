c71ee2d6fc0df39d7f3bd38aaf93d1f411ac0b21
Jose Ferreira <jferreira590@gmail.com>
2020-06-04 12:31:45 -0400

    Adding Dockerfiles and setting up docker-compose.

--- server/db/index.js
+++ server/db/index.js
@@ -1,14 +1,14 @@
 const mongoose = require('mongoose');
 
 let MONGODB_URI =
-  process.env.PROD_MONGODB || 'mongodb://127.0.0.1:27017/redSquirrelDatabase';
+  process.env.PROD_MONGODB || 'mongodb://mongo:27017/redSquirrelDatabase';
 
 mongoose
   .connect(MONGODB_URI, { useUnifiedTopology: true, useNewUrlParser: true })
   .then(() => {
     console.log('Successfully connected to MongoDB.');
   })
-  .catch((e) => {
+  .catch(e => {
     console.error('Connection error', e.message);
   });
 
